c841e0e9c0bfe2ead727a584edc4c2957ad80f46
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-08-13 14:18:54 +0530

    commit

---
+++ Project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='header'>
+        header
+      </div>
+      <div className='body'>
+        body
+      </div>
+      <div className='footer'>
+        footer
+      </div>
+    </div>
+  )
+}
+
+export default App
